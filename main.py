39bd32a186e72bfb8913215f998c370afbdfcbf6
Mr.Ülg3n <68772806+SwazzAtena@users.noreply.github.com>
2023-03-26 18:17:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from pynput.keyboard import Key, Controller
+keyboard = Controller()
+
+from pynput.mouse import Button, Controller
+mouse = Controller()
+
+import pyautogui
+import random
+import time
+z = random.randint(2,6)
+y = random.randint(18 , 32)
+x = 0
+while True:
+    pyautogui.moveTo(157,14)
+    pyautogui.click(clicks=1)
+    pyautogui.moveTo(489,1002)
+    pyautogui.click(clicks=1)
+    pyautogui.typewrite("owo hunt", interval=0.10)
+    pyautogui.press("enter")
+    time.sleep(z)
+    time.sleep(11)
+    x = x + 2
